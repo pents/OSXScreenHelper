6a891e383a7b3e08516d70a46de1b4684a8ce6c8
Davydov Dmitry <dmitdavydov@ozon.ru>
2022-12-08 11:58:28 +0300

    added saving in png

--- OSX_Screen/ScreenHelper.cpp
+++ OSX_Screen/ScreenHelper.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "ScreenHelper.h"
+#include <UniformTypeIdentifiers/UniformTypeIdentifiers.h>
+#include <string>
 #include <CoreGraphics/CGDisplayConfiguration.h>
 #include <ApplicationServices/ApplicationServices.h>
 
@@ -36,4 +38,55 @@ Screenshot GetScreenshot()
     CGImageRelease(image_ref);
 
     return {(int)width, (int)height, (int)bpp, buffer};
+}
+
+bool SaveToFile(int8_t* buffer, uint16_t width, uint16_t height, std::string filename)
+{
+    bool status = false;
+
+    CGDataProviderRef provider_ref = CGDataProviderCreateWithData(NULL, buffer, width * height * 4, NULL);
+    if (provider_ref != NULL) {
+        CGColorSpaceRef cs_ref = CGColorSpaceCreateDeviceRGB();
+        if (cs_ref != nullptr) {
+            CGImageRef img_ref = CGImageCreate(
+                width,
+                height,
+                8,          // bitsPerComponent
+                32,         // bitsPerPixel
+                width * 4,  // bytesPerRow
+                cs_ref,
+                kCGBitmapByteOrderDefault,
+                provider_ref,
+                NULL,       // decode array
+                false,      // shouldInterpolate
+                kCGRenderingIntentDefault
+            );
+
+            if (img_ref != NULL) {
+                CFStringRef folderCFStr = CFStringCreateWithCString(0, filename.data(), kCFStringEncodingUTF8);
+                CFURLRef url_ref = CFURLCreateWithFileSystemPath(kCFAllocatorDefault, folderCFStr, kCFURLPOSIXPathStyle, false);
+                if (url_ref != NULL) {
+                    CGImageDestinationRef destination_ref = CGImageDestinationCreateWithURL(url_ref, UTTypePNG, 1, NULL);
+                    if (destination_ref != NULL) {
+                        CGImageDestinationAddImage(destination_ref, img_ref, NULL);
+                        if (CGImageDestinationFinalize(destination_ref)) {
+                            status = true;
+                        }
+
+                        CFRelease(destination_ref);
+                    }
+
+                    CFRelease(url_ref);
+                }
+
+                CGImageRelease(img_ref);
+            }
+
+            CGColorSpaceRelease(cs_ref);
+        }
+
+        CGDataProviderRelease(provider_ref);
+    }
+
+    return status;
 }
